b914a41f3379423ca9596bbbb7a74d3c53ee7b63
Craig McKeachie <craigdmckeachie@gmail.com>
2015-10-30 08:39:23 -0400

    Services: Provider

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
 (function () {
     "use strict";
 
-    //object literal
-    var DroidValue = {
-        name: '',
-        speak: function () {
-            return "Hi I am " + this.name;
+    //configurable module
+    function DroidProvider() {
+        var greeting = '';
+        return{
+            configure: function (settings) {
+                greeting = settings.greeting;
+            },
+            $get: function () {
+                return{
+                    name: '',
+                    speak: function () {
+                        return greeting + this.name;
+                    }
+                }
+            }
         }
-    };
+    }
 
-    var droid = DroidValue;
-    droid.name = "c3-po";
+    var droidProvider = DroidProvider();
+    droidProvider.configure({greeting: 'Greetings I am '});
+    var droid = droidProvider.$get();
+    droid.name = 'ig-88';
     console.log(droid.speak());
-
 
     /* Angular Stuff*/
     angular.module('app', [])
-        .value('droid', DroidValue)
+        .config(configure)
+        .provider('droid', DroidProvider)
         .controller('DroidController', DroidController)
 
     function DroidController(droid) {
         var droidCtrl = this;
-        droid.name = 'c3-po';
+        droid.name = 'ig-88'
         droidCtrl.message = droid.speak();
+
+    }
+
+    function configure(droidProvider) {
+        droidProvider.configure({greeting: 'Greetings I am '});
     }
 
 
