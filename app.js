dcd8a7957d749a22339513d3f08d5466a48183d4
Craig McKeachie <craigdmckeachie@gmail.com>
2015-10-31 09:07:37 -0400

    Services: constant

--- app.js
+++ app.js
@@ -1,87 +1,32 @@
 (function () {
     "use strict";
 
-    //constructor function
-    function DroidService() {
-        this.name = ''
-    }
-
-    DroidService.prototype.speak = function () {
-        return "Hi I am " + this.name;
-    }
-
-    var droid1 = new DroidService();
-    droid1.name = 'r2-d2';
-
-    console.log(droid1.speak());
-
-    //////////////////////////////////////////////////////
-    //revealing module pattern
-    function DroidFactory() {
-
-        function speak() {
-            return "Hi I am " + this.name;
-        }
-
-        return {
-            name: '',
-            speak: speak
-        }
-    }
-
-    var droid2 = DroidFactory();
-    droid2.name = "bb-8";
-    console.log(droid2.speak());
-
-    //////////////////////////////////////////////////////
-    //value
-
-    var DroidValue  = {name: '',
-        speak: function () {
-            return "I am " + this.name;
-        }
+    //constant
+    var errors = {
+        mindtrick: "These aren't the droids you're looking for"
     };
+    
+    console.log(errors.mindtrick);
 
-    var droid3 = DroidValue;
-    droid3.name = "c3-po";
-    console.log(droid3.speak());
-
-    //////////////////////////////////////////////////////
-    //module pattern (configurable per app)
-    function DroidProvider() {
-        var greeting;
-        return{
-            configure: function (settings) {
-                greeting = settings.greeting;
-            },
-            $get: function () {
-                return{
-                    name: '',
-                    speak: function () {
-                        return greeting + this.name;
-                    }
-
-                }
-            }
-        }
-    }
-
-    var droidProvider = DroidProvider();
-    droidProvider.configure({greeting: "Hi I am "});
-    var droid = droidProvider.$get();
-    droid.name = 'ig-88';
-    console.log(droid.speak());
 
     //////////////////////////////////////////////////////
     /* Angular Stuff*/
     angular.module('app', [])
-        .value('droid', DroidValue)
+        .constant('errors', errors)
+        .config(configure)
         .controller('DroidController', DroidController)
 
-    function DroidController(droid) {
+    function DroidController(errors) {
         var droidCtrl = this;
-        droid.name = 'ig-88';
-        droidCtrl.message = droid.speak();
+        errors.mindtrick = "another error";
+        droidCtrl.message = errors.mindtrick;
+
+    }
+
+    function configure(errors) {
+        console.log("in config: " + errors.mindtrick);
+        
+        
     }
 
 
